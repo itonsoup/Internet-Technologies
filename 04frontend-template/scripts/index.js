8c4e398e6579b8a1fbb92efeefdc0bab292648bb
itonsoup <145210725+itonsoup@users.noreply.github.com>
2024-12-08 18:42:41 +0300

    Добавил вывод 6 карточек
    с помощью js реализовал автоматический вывод карточек с сервера на сайт

--- 04frontend-template/scripts/index.js
+++ 04frontend-template/scripts/index.js
@@ -8,4 +8,29 @@
 
 // @todo: Вывести карточки на страницу
 
+import { initialCards } from './cards.js';
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+function createCard({ name, link }) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardTitle.textContent = name;
+
+  return cardElement;
+}
+
+function renderInitialCards(cards) {
+  cards.forEach((card) => {
+    const cardElement = createCard(card);
+    placesList.append(cardElement);
+  });
+}
+
+renderInitialCards(initialCards);
   

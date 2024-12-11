967456ed7ee5c86860d6a3b97eaaa4ca43092efc
itonsoup <145210725+itonsoup@users.noreply.github.com>
2024-12-11 22:53:07 +0300

    Update cards.js

--- 04frontend-template/src/scripts/cards.js
+++ 04frontend-template/src/scripts/cards.js
@@ -1,26 +1,26 @@
 export const initialCards = [
-    {
-      name: "Сентинский храм",
-      link: "http://127.0.0.1:55215/card_1.jpg",
-    },
-    {
-      name: "Кавказ",
-      link: "http://127.0.0.1:55215/card_2.jpg",
-    },
-    {
-      name: "Домбай",
-      link: "http://127.0.0.1:55215/card_3.jpg",
-    },
-    {
-      name: "Долина роз",
-      link: "http://127.0.0.1:55215/card_4.jpg",
-    },
-    {
-      name: "Эльбрус",
-      link: "http://127.0.0.1:55215/card_5.jpg",
-    },
-    {
-      name: "Долина нарзанов",
-      link: "http://127.0.0.1:55215/card_6.jpg",
-    }
+  {
+    name: "Сентинский храм",
+    link: "http://127.0.0.1:55215/card_1.jpg",
+  },
+  {
+    name: "Кавказ",
+    link: "http://127.0.0.1:55215/card_2.jpg",
+  },
+  {
+    name: "Домбай",
+    link: "http://127.0.0.1:55215/card_3.jpg",
+  },
+  {
+    name: "Долина роз",
+    link: "http://127.0.0.1:55215/card_4.jpg",
+  },
+  {
+    name: "Эльбрус",
+    link: "http://127.0.0.1:55215/card_5.jpg",
+  },
+  {
+    name: "Долина нарзанов",
+    link: "http://127.0.0.1:55215/card_6.jpg",
+  }
 ];

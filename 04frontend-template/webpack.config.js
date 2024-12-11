69afff6bda14ef1a5307ea22f23caa5378e2c1ae
itonsoup <145210725+itonsoup@users.noreply.github.com>
2024-12-11 22:39:23 +0300

    dev

--- 04frontend-template/webpack.config.js
+++ 04frontend-template/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/dist/',
-    filename: "bundle.js",
+    filename: "main.js",
     chunkFilename: '[name].js'
   },
   module: {

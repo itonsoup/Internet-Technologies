4430640d27bf1de67de944ec64b13e1314acc3de
itonsoup <145210725+itonsoup@users.noreply.github.com>
2024-12-11 23:07:12 +0300

    Update webpack.config.js

--- 04frontend-template/webpack.config.js
+++ 04frontend-template/webpack.config.js
@@ -1,43 +1,36 @@
 const path = require('path');
+
 module.exports = {
   mode: 'development',
   entry: path.join(__dirname, 'src', 'scripts', 'index.js'),
-  watch: true,
+  watch: false,
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: '/dist/',
-    filename: "main.js",
-    chunkFilename: '[name].js'
+    filename: 'main.js'
   },
   module: {
-    rules: [{
-      test: /.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'src')
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules')
-      ],
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          ["@babel/preset-env", {
-            "targets": "> 0.25%, not dead"
-          }]
-        ]        
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }]
+            ]
+          }
+        }
       }
-    }]
-  },
-  resolve: {
-    extensions: ['.json', '.js', '.jsx']
+    ]
   },
   devtool: 'source-map',
   devServer: {
-    static: path.join(__dirname, 'dist'), 
-    host: 'localhost', 
-    port: 8080, 
-    open: true, 
-    hot: true, 
-    liveReload: true 
+    static: path.join(__dirname, 'dist'),
+    host: 'localhost',
+    port: 8080,
+    open: true,
+    hot: true,
+    liveReload: true
   }
 };
